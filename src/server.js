ea1bb2bacb3af28992371a0188edcad19a7cd857
Paula Rolim <rolim.paula1999@gmail.com>
2022-07-31 15:39:31 -0300

    refactor: port server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { router } = require("./routes/v1/funds.routes");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/v1", router);
 
