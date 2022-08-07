af4a97fa7a95cac0a41f0febbe24958c80cdcfeb
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-07 11:44:57 -0300

    ci: add headless to puppeteer

--- src/scraping/config.js
+++ src/scraping/config.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer-core");
 
 const getPageAndBrower = async (url) => {
   const browser = await puppeteer.launch({
-    // headless: false,
+    headless: true,
     // executablePath: "./node_modules/chromium/lib/chromium/chrome-linux/chrome",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
